2edb6a92c9340f84f614bd5d0e5526194c11c16a
jisung729 <kscaw@naver.com>
2019-04-08 17:12:56 +0900

    메인페이지 모바일 메뉴버튼 클릭시 열고 닫고 자바스크립트 완성

--- new.js
+++ new.js
@@ -19,8 +19,38 @@ $("menu_open").click(function(){
     getElementsByName("samsna_backg").css({"display":"block"})
 })
 */
+
+function menu_open(){
+    if(document.querySelector('#menu_open').value === 'menu'){
+        document.querySelector('.samsna_backg').style.display='block';
+        document.querySelector('#menu_open').value = 'off';
+    }else{
+        document.querySelector('.samsna_backg').style.display='none';
+        document.querySelector('#menu_open').value = 'menu';
+    };
+};
+
+/*
+if(document.querySelector("#menu_open").value === 'menu'){
 $('#menu_open').click(function(){
     $('.samsna_backg').css('display','block');
+    document.querySelector("#menu_open").value = 'off'
 });
 
+}else{
+$('#menu_open').click(function(){
+    $(".samsna_backg").css('display','none');
+    document.querySelector("#menu_open").value = 'menu'
+});
+};
 
+if(){
+    $('#menu_open').click(function(){
+        $('.samsna_backg').css('display','block');
+});
+    }else{
+    $('#menu_open').click(function(){
+        $(".samsna_backg").css('display','none');
+});
+}
+*/
